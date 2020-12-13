8eac16d07a9d24e27734fd609bd2cb34be5ccfbf
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2020-12-14 00:07:11 +0600

    add sampl reactive forms

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -7,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+  /**
+   * Init form
+   */
+  form = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required
+    ]),
+  });
+
+  get f(): any {
+    return this.form.controls;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Submit form
+   */
+  onSubmit(): void {
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    const formData = this.form.value;
+    console.log(this.form.value)
+
   }
 
 }
